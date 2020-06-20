a230efd18f91f61c06e86e30607f5943ca62b744
alexguirre <alexguirre.aa@gmail.com>
2020-06-20 06:43:04 +0200

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ScTools
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
