2bece3e20a1bec2bbfd3a9cb93220aa1ff67b901
alexguirre <alexguirre.aa@gmail.com>
2022-06-10 17:27:36 +0200

    Move JSON converter out of ProjectConfiguration

---
+++ src/Core/ScriptLang/Workspace/Json/BuildConfigurationsConverter.cs
@@ -0,0 +1,44 @@
+﻿namespace ScTools.ScriptLang.Workspace.Json;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Serializes the array of build configurations as a JSON object where the keys are the names.
+/// </summary>
+public sealed class BuildConfigurationsConverter : JsonConverter<ImmutableArray<BuildConfiguration>>
+{
+    /// <summary>
+    /// Same as <see cref="BuildConfiguration"/> but without <see cref="BuildConfiguration.Name"/>.
+    /// </summary>
+    private record BuildConfigurationWithoutName(BuildTarget Target, ImmutableArray<string> Defines);
+
+    public override ImmutableArray<BuildConfiguration> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType is not JsonTokenType.StartObject) { throw new JsonException(); }
+        reader.Read();
+
+        var buildConfigs = ImmutableArray.CreateBuilder<BuildConfiguration>();
+        while (reader.TokenType is not JsonTokenType.EndObject)
+        {
+            var name = reader.GetString() ?? throw new JsonException("Build configuration name cannot be null");
+            reader.Read();
+            var tempConfig = JsonSerializer.Deserialize<BuildConfigurationWithoutName>(ref reader, options) ?? throw new JsonException("Build configuration cannot be null");
+            reader.Read();
+            buildConfigs.Add(new(name, tempConfig.Target, tempConfig.Defines));
+        }
+
+        return buildConfigs.ToImmutable();
+    }
+
+    public override void Write(Utf8JsonWriter writer, ImmutableArray<BuildConfiguration> value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        foreach (var buildConfig in value)
+        {
+            writer.WritePropertyName(buildConfig.Name);
+            JsonSerializer.Serialize(writer, new BuildConfigurationWithoutName(buildConfig.Target, buildConfig.Defines), options);
+        }
+        writer.WriteEndObject();
+    }
+}
--- src/Core/ScriptLang/Workspace/ProjectConfiguration.cs
+++ src/Core/ScriptLang/Workspace/ProjectConfiguration.cs
@@ -6,7 +6,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 public record ProjectConfiguration(
-    [property: JsonConverter(typeof(ProjectConfiguration.BuildConfigurationsJsonConverter))]
+    [property: JsonConverter(typeof(Json.BuildConfigurationsConverter))]
     ImmutableArray<BuildConfiguration> BuildConfigurations)
 {
     public BuildConfiguration? GetBuildConfiguration(string name) => BuildConfigurations.FirstOrDefault(c => c.Name == name);
@@ -40,44 +40,4 @@ public record ProjectConfiguration(
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = true,
     };
-
-    /// <summary>
-    /// Serializes the array of build configurations as a dictionary where the key is the name.
-    /// </summary>
-    public sealed class BuildConfigurationsJsonConverter : JsonConverter<ImmutableArray<BuildConfiguration>>
-    {
-        /// <summary>
-        /// Same as <see cref="BuildConfiguration"/> but without <see cref="BuildConfiguration.Name"/>.
-        /// </summary>
-        private record BuildConfigurationWithoutName(BuildTarget Target, ImmutableArray<string> Defines);
-
-        public override ImmutableArray<BuildConfiguration> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            if (reader.TokenType is not JsonTokenType.StartObject) { throw new JsonException(); }
-            reader.Read();
-
-            var buildConfigs = ImmutableArray.CreateBuilder<BuildConfiguration>();
-            while (reader.TokenType is not JsonTokenType.EndObject)
-            {
-                var name = reader.GetString() ?? throw new JsonException("Build configuration name cannot be null");
-                reader.Read();
-                var tempConfig = JsonSerializer.Deserialize<BuildConfigurationWithoutName>(ref reader, options) ?? throw new JsonException("Build configuration cannot be null");
-                reader.Read();
-                buildConfigs.Add(new(name, tempConfig.Target, tempConfig.Defines));
-            }
-
-            return buildConfigs.ToImmutable();
-        }
-
-        public override void Write(Utf8JsonWriter writer, ImmutableArray<BuildConfiguration> value, JsonSerializerOptions options)
-        {
-            writer.WriteStartObject();
-            foreach (var buildConfig in value)
-            {
-                writer.WritePropertyName(buildConfig.Name);
-                JsonSerializer.Serialize(writer, new BuildConfigurationWithoutName(buildConfig.Target, buildConfig.Defines), options);
-            }
-            writer.WriteEndObject();
-        }
-    }
 }
