258a350efbe7f8eab65e63c508abeee2742a3f46
alexguirre <alexguirre.aa@gmail.com>
2023-02-15 22:03:07 +0100

    Move NativeType to TypeInfo.cs

--- src/Core/ScriptLang/Types/TypeInfo.cs
+++ src/Core/ScriptLang/Types/TypeInfo.cs
@@ -101,6 +101,20 @@ public sealed record VectorType : TypeInfo
         new FieldInfo(FloatType.Instance, "z", 2));
 }
 
+/// <summary>
+/// Strongly typed integer that represents a handle to a native object.
+/// </summary>
+/// <param name="Declaration">Declaration of this native type.</param>
+/// <param name="Base">Base type of this native type. This makes this native type implicitly convertible to the base native type. For example PED_INDEX/VEHICLE_INDEX/OBJECT_INDEX to ENTITY_INDEX.</param>
+public sealed record NativeType(NativeTypeDeclaration Declaration, NativeType? Base) : TypeInfo
+{
+    public override int SizeOf => 1;
+    public override ImmutableArray<FieldInfo> Fields => ImmutableArray<FieldInfo>.Empty;
+
+    public override string ToPrettyString() => Declaration.Name;
+    public override TReturn Accept<TReturn>(ITypeVisitor<TReturn> visitor) => visitor.Visit(this);
+}
+
 public sealed record EnumType(EnumDeclaration Declaration) : TypeInfo
 {
     public override int SizeOf { get; } = 1;
